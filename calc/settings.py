216848d6c3d98f18ef53a873d3a5ddadcc537350
unknown <samuellpvt@gmail.com>
2019-08-21 16:55:29 -0300

    heroku

--- calc/settings.py
+++ calc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.4','0.0.0.0','localhost']
 
-
+django_heroku.settings(locals())
 # Application definition
 
 INSTALLED_APPS = [
